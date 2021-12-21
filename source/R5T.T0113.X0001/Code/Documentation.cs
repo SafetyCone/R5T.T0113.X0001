7854b321672e011d8cf01e35aef4bd13c32c1b2d
davidcoats <david@getsafetycone.com>
2021-12-21 12:15:18 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0113.X0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0113.X0001
+{
+    /// <summary>
+    /// IProjectOperator, ISolutionOperator, and related extension method base public, common, Rivet-style extensions.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
